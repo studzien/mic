f1d57afac0af17566a37ed78224ab3d5149a53b8
Filip Dziedzic <filip@mint>
2013-11-26 20:27:58 +0100

    initial project

---
+++ src/pl/edu/agh/mic/services/TestService.java
@@ -0,0 +1,19 @@
+package pl.edu.agh.mic.services;
+
+import javax.jws.WebService;
+
+@WebService(
+        portName = "TestPort",
+        serviceName = "TestService",
+        targetNamespace = "http://superbiz.org/wsdl",
+        endpointInterface = "org.superbiz.calculator.ws.CalculatorWs")
+public class TestService implements TestServiceWs {
+
+    public int sum(int add1, int add2) {
+        return add1 + add2;
+    }
+
+    public int multiply(int mul1, int mul2) {
+        return mul1 * mul2;
+    }
+}
---
+++ src/pl/edu/agh/mic/services/TestServiceWs.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.mic.services;
+
+import javax.jws.WebService;
+
+@WebService(targetNamespace = "http://superbiz.org/wsdl")
+public interface TestServiceWs {
+
+    public int sum(int add1, int add2);
+
+    public int multiply(int mul1, int mul2);
+}
